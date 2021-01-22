52213e980cfe96371019de8c606dc154cb4b4ffe
MHanson15 <hansonmatthew22@gmail.com>
2021-01-22 15:28:58 -0800

    Removed news ticker, fixed ROH, added NJPW KOPW title, added NXT UK

--- wrestling_roster_tool.py
+++ wrestling_roster_tool.py
@@ -3,14 +3,6 @@ from tkinter import *
 from tkinter import ttk
 from company import *
 
-def showNews():
-	i = tab_parent.index(tab_parent.select())
-	if(i < 6):
-		tabs[i].newsTicker()
-	else:
-		i = wwe.index(wwe.select())
-		wwe_tabs[i].newsTicker()
-	window.after(300, showNews)
 def save():
 	for company in tabs:
 		company.save()
@@ -42,35 +34,36 @@ menu.add_cascade(label="File", menu=file)
 tab_parent = ttk.Notebook(window)
 	#Create companies
 tabs = [
-Company("AAA",'https://en.wikipedia.org/wiki/List_of_Lucha_Libre_AAA_Worldwide_personnel',tab_parent,[0],[1],
+Company("AAA",'https://en.wikipedia.org/wiki/List_of_Lucha_Libre_AAA_Worldwide_personnel',tab_parent,
 ["AAA Mega Champion","AAA Latin American Champion","AAA World Cruiserweight Champion","AAA Reina de Reinas Champion", 
-"AAA World Tag Team Champion","AAA World Trios Champion","AAA World Mixed Tag Team Champion"],'https://www.f4wonline.com/mexico-news'),
-Company("AEW",'https://en.wikipedia.org/wiki/List_of_All_Elite_Wrestling_personnel',tab_parent,[0],[1],[
-"World Champion","Women's World Champion","TNT Champion","World Tag Team Champion","FTW Champion"],'https://www.f4wonline.com/aew-news'),
-Company("Impact",'https://en.wikipedia.org/wiki/List_of_Impact_Wrestling_personnel',tab_parent,[0],[1],[
-"World Champion","Knockouts Champion","X Division Champion","World Tag Team Champion"],'https://www.f4wonline.com/tna-news'),
-MLW("MLW",'https://en.wikipedia.org/wiki/List_of_Major_League_Wrestling_personnel',tab_parent,[0],[1],[
-"MLW World Heavyweight Champion","MLW World Middleweight Champion","MLW National Openweight Champion","MLW World Tag Team Champion"],'https://www.f4wonline.com/tags/mlw'),
-NXT("NWA",'https://en.wikipedia.org/wiki/List_of_National_Wrestling_Alliance_personnel',tab_parent,[0],[1],[
-"NWA Worlds Heavyweight Champion","NWA World Women's Champion","NWA National Heavyweight Champion","NWA World Tag Team Champion"],'https://www.f4wonline.com/tags/nwa'),
-NJPW("NJPW",'https://en.wikipedia.org/wiki/List_of_New_Japan_Pro-Wrestling_personnel',tab_parent,[0],[1],["IWGP Heavyweight Champion",
+"AAA World Tag Team Champion","AAA World Trios Champion","AAA World Mixed Tag Team Champion"]),
+Company("AEW",'https://en.wikipedia.org/wiki/List_of_All_Elite_Wrestling_personnel',tab_parent,[
+"World Champion","Women's World Champion","TNT Champion","World Tag Team Champion","FTW Champion"]),
+Company("Impact",'https://en.wikipedia.org/wiki/List_of_Impact_Wrestling_personnel',tab_parent,[
+"World Champion","Knockouts Champion","X Division Champion","World Tag Team Champion"]),
+MLW("MLW",'https://en.wikipedia.org/wiki/List_of_Major_League_Wrestling_personnel',tab_parent,[
+"MLW World Heavyweight Champion","MLW World Middleweight Champion","MLW National Openweight Champion","MLW World Tag Team Champion"]),
+Company("NWA",'https://en.wikipedia.org/wiki/List_of_National_Wrestling_Alliance_personnel',tab_parent,[
+"NWA Worlds Heavyweight Champion","NWA World Women's Champion","NWA National Heavyweight Champion","NWA World Tag Team Champion"]),
+NJPW("NJPW",'https://en.wikipedia.org/wiki/List_of_New_Japan_Pro-Wrestling_personnel',tab_parent,["IWGP Heavyweight Champion",
 "IWGP Intercontinental Champion","IWGP United States Champion","NEVER Openweight Champion","IWGP Tag Team Champion",
-"IWGP Junior Heavyweight Champion","IWGP Junior Heavyweight Tag Team Champion","NEVER Openweight 6-Man Tag Team Champion"],'https://www.f4wonline.com/japan-news'),
-Company("ROH",'https://en.wikipedia.org/wiki/List_of_Ring_of_Honor_personnel',tab_parent,[0],[1],[
-"ROH World Champion","ROH World Television Champion","ROH World Tag Team Champion","ROH World Six-Man Tag Team Champion"],'https://www.f4wonline.com/roh-news')
+"IWGP Junior Heavyweight Champion","IWGP Junior Heavyweight Tag Team Champion","NEVER Openweight 6-Man Tag Team Champion", "KOPW 2020"]),
+Company("ROH",'https://en.wikipedia.org/wiki/List_of_Ring_of_Honor_personnel',tab_parent,[
+"World Champion","Pure Wrestling Champion","Television Champion","World Tag Team Champion","World Six-Man Tag Team Champion"])
 ]
 	#WWE contains brands so it becomes a parent tab
 wwe = ttk.Notebook(tab_parent)
 	#Create brands
 wwe_tabs = [
-WWE("RAW",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[1],[2],[
-"WWE Champion","Raw Women's Champion","United States Champion","Raw Tag Team Champion","24/7 Champion"],"Raw",'https://www.f4wonline.com/wwe-news'),
-WWE("Smackdown",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[4],[5],[
+WWE("RAW",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
+"WWE Champion","Raw Women's Champion","United States Champion","Raw Tag Team Champion","24/7 Champion"],"Raw"),
+WWE("Smackdown",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
 "Universal Champion","SmackDown Women's Champion","Intercontinental Champion","SmackDown Tag Team Champion",
-"Women's Tag Team Champion"],"SmackDown",'https://www.f4wonline.com/wwe-news'),
-WWE("NXT",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[7,10,13],[8,11],[
-"NXT Champion","NXT Women's Champion","North American Champion","NXT Tag Team Champion","NXT Cruiserweight Champion","NXT United Kingdom Champion",
-"NXT UK Women's Champion","NXT UK Tag Team Champion"], "NXT",'https://www.f4wonline.com/tags/nxt')
+"Women's Tag Team Champion"],"SmackDown"),
+WWE("NXT",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
+"NXT Champion","NXT Women's Champion","North American Champion","NXT Tag Team Champion","NXT Cruiserweight Championship"], "NXT"),
+WWE("NXT UK",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
+"NXT United Kingdom Champion","NXT UK Women's Champion","NXT UK Tag Team Champion", "NXT Cruiserweight Champion", "Heritage Cup Champion"], "NXT_UK")
 ]
 
 tab_parent.add(tabs[0].frame, text=tabs[0].name)
@@ -79,10 +72,12 @@ tab_parent.add(tabs[2].frame, text=tabs[2].name)
 tab_parent.add(tabs[3].frame, text=tabs[3].name)
 tab_parent.add(tabs[4].frame, text=tabs[4].name)
 tab_parent.add(tabs[5].frame, text=tabs[5].name)
+tab_parent.add(tabs[6].frame, text=tabs[6].name)
 tab_parent.add(wwe,text="WWE")
 wwe.add(wwe_tabs[0].frame,text=wwe_tabs[0].name)
 wwe.add(wwe_tabs[1].frame, text=wwe_tabs[1].name)
 wwe.add(wwe_tabs[2].frame, text=wwe_tabs[2].name)
+wwe.add(wwe_tabs[3].frame, text=wwe_tabs[3].name)
 
 tab_parent.pack(expand=1, fill='both')
 
@@ -90,5 +85,4 @@ tab_parent.pack(expand=1, fill='both')
 load()
 
 #main loop
-showNews()
 window.mainloop()
