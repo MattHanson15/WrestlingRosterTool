1e0a2544f691748a903a8c721c72b1db01a75a29
MHanson15 <hansonmatthew22@gmail.com>
2021-01-24 14:49:11 -0800

    Modified to scrape from Cagematch, no saving/loading or champions yet

--- company.py
+++ company.py
@@ -13,211 +13,103 @@ class Company:
         self.belts = belts
         self.champs = []
         self.frame = ttk.Frame(parent_tab)
-        self.listbox_male = Listbox(self.frame, width=35)
-        self.listbox_female = Listbox(self.frame, width=35)
+        self.listbox_roster = Listbox(self.frame, width=35)
         self.button_update = Button(self.frame, text="Update", command=self.update)
         self.label_champHeader = Label(self.frame, text="Current Champions")
-        self.label_men = Label(self.frame, text="Men")
-        self.label_women = Label(self.frame, text="Women")
+        self.label_roster = Label(self.frame, text="Roster")
         
-        self.label_men.grid(row=1, column=0, padx=(90,0), pady=(5,0), sticky=W)
-        self.label_women.grid(row=1,column=1, padx=(90,0), pady=(5,0), sticky=W)
-        self.listbox_male.grid(row=1+1, column=0, padx=(15,0), pady=(0,30), sticky=W)
-        self.listbox_female.grid(row=1+1, column=1, padx=(0,0), pady=(0,30), sticky=W)
-        self.button_update.grid(row=2+1, column=0, padx=(0,80), sticky=E)
-        self.label_champHeader.grid(row=3+1,column=0, padx=15, pady=5)
-        self.frame.columnconfigure(0, weight=1)
-        self.frame.columnconfigure(1, weight=1)
-        self.frame.columnconfigure(2, weight=1)
+        self.label_roster.grid(row=1, column=0, padx=(90,0), pady=(5,0), sticky=W)
+        self.listbox_roster.grid(row=2, column=0, padx=(15,0), pady=(0,30), sticky=W)
+        self.button_update.grid(row=3, column=0, padx=(0,80), sticky=E)
+        self.label_champHeader.grid(row=4,column=0, padx=15, pady=5)
         i=0
         while i < len(self.belts):
             self.champs.append(Label(self.frame,text=self.belts[i]+": "))
             self.champs[i].grid(row=4+i+1, column=0, columnspan=2, padx=15, pady=2, sticky=W)
-            i+=1
-    #Methods
-    def getWrestlers(self, soup, category, listbox):
-        listbox.delete(0,END)
-        table = soup.find(id=category).findNext('table')
-        rows = table.findAll('tr')
-        for row in rows:
-            data = row.findAll('td')
-            if(len(data) > 0):
-                name = data[0].text.strip().split('[')[0]
-                listbox.insert(END, name)
-                notes = data[2].findAll('a')
-                if(len(notes) > 0):
-                    k=0
-                    for belt in self.belts:
-                        for note in notes:
-                            if (note.text.strip().split('[')[0] == belt or note.get('title') == belt ):
-                                self.champs[k].configure(text=self.champs[k]["text"]+name+"  ")
-                        k = k + 1
+            i+=1    
     def update(self):
         try:
-            page = requests.get(self.url)
+            page = requests.get(self.url, headers={'Accept-Encoding': 'identity'})
             soup = BeautifulSoup(page.content, 'html.parser')
         except:
             print("Error occured while updating")
             return()
-        i=0
-        while i < len(self.champs):
-            self.champs[i].configure(text=self.belts[i]+": ")
-            i+=1
-        self.getWrestlers(soup, "Male_wrestlers", self.listbox_male)
-        self.getWrestlers(soup,"Female_wrestlers",self.listbox_female)
-    def save(self):
-        found = 0
-        count = 0
-        male_roster = ""
-        female_roster = ""
-        champ_roster = ""
         
-        for wrestler in self.listbox_male.get(0,END):
-            if(count > 0):
-                male_roster = male_roster + ","+wrestler
-            else:
-                male_roster = wrestler
-            count += 1
-        count = 0
-        for wrestler in self.listbox_female.get(0,END):
-            if(count > 0):
-                female_roster = female_roster + ","+wrestler
-            else:
-                female_roster = wrestler
-            count += 1
-        count = 0
-        for wrestler in self.champs:
-            if(count > 0):
-                champ_roster = champ_roster + ","+wrestler['text']
-            else:
-                champ_roster = wrestler['text']
-            count += 1
-        count = 0
-        try:
-            f = open('data.csv', 'r')
-            contents = f.readlines()
-            f.close()
-        except:
-            contents=[]
-        for line in contents:
-            if(found == 0):
-                if(self.name == line.strip()):
-                    found = 1
-                count +=1
-        print("Saving local "+self.name+" data")
-        if(found == 1):
-            contents[count]=male_roster+'\n'
-            contents[count+1]=female_roster+'\n'
-            contents[count+2]=champ_roster+'\n'
-        if(found == 0):
-            contents.append(self.name+'\n')
-            contents.append(male_roster+'\n')
-            contents.append(female_roster+'\n')
-            contents.append(champ_roster+'\n')
+        i=0
+        while i < len(self.champs):
+            self.champs[i].configure(text=self.belts[i]+": ")
+            i+=1
+        
+        self.listbox_roster.delete(0,END)
+        
+        table = soup.find('table')
+        if not (table):
+            print("Could not find table")
+            return()
             
-        f = open('data.csv', 'w')
-        f.writelines(contents)
-        f.close()
-    def load(self):
-        try:
-            f = open('data.csv', 'r+', newline='')
-        except:
-            print("Unable to load data.csv")
+        rows = table.findAll('tr')
+        if not (rows):
+            print("Found table but no rows")
             return()
-        count=0
-        found=0
-        csv_reader = csv.reader(f, delimiter=',')
-        for row in csv_reader:
-            if(found == 0):
-                if(len(row) > 0):
-                    if (self.name == row[0].strip()):
-                        found=1
-                        print("Loading local "+self.name+" data")
-                        break
-        if(found == 1):
-            for wrestler in next(csv_reader):
-                self.listbox_male.insert(END,wrestler)
-            for wrestler in next(csv_reader):
-                self.listbox_female.insert(END,wrestler)
-            for wrestler in next(csv_reader):
-                self.champs[count].configure(text=self.champs[count]['text']+wrestler.split(": ")[1])
-                count+=1
+        
+        for row in rows:
+            cells = row.findAll('td')
+            if not (cells):
+                continue
+            if(len(cells) < 4):
+                continue
+            if(cells[3].text.find("Wrestler") >= 0):
+                self.listbox_roster.insert(END, cells[2].text)
 
-class MLW(Company):
+class WWE():
+    def __init__(self, parent_tab):
+        self.name = "WWE"
+        self.url = "https://www.cagematch.net/?id=8&nr=1&page=15"
+        self.notebook = ttk.Notebook(parent_tab)
+        self.brands = [
+                Company("RAW"," ",self.notebook,[
+                    "WWE Champion","Raw Women's Champion","United States Champion","Raw Tag Team Champion","Women's Tag Team Champion","24/7 Champion"]),
+                Company("Smackdown"," ",self.notebook,[
+                    "Universal Champion","SmackDown Women's Champion","Intercontinental Champion","SmackDown Tag Team Champion",
+                    "Women's Tag Team Champion","24/7 Champion"]),
+                Company("NXT"," ",self.notebook,[
+                    "NXT Champion","NXT Women's Champion","North American Champion","NXT Tag Team Champion","Women's Tag Team Champion","NXT Cruiserweight Championship"]),
+                Company("NXT UK"," ",self.notebook,[
+                    "NXT United Kingdom Champion","NXT UK Women's Champion","NXT UK Tag Team Champion", "NXT Cruiserweight Champion", "Heritage Cup Champion"])
+                ]
+        for brand in self.brands:
+            self.notebook.add(brand.frame,text=brand.name)
+            brand.button_update.configure(command=self.update)
+            i=0    
+            while i < len(brand.champs):
+                brand.champs[i].configure(text=brand.belts[i]+": ")
+                i+=1   
     def update(self):
         try:
-            page = requests.get(self.url)
+            page = requests.get("https://www.cagematch.net/?id=8&nr=1&page=15", headers={'Accept-Encoding': 'identity'})
             soup = BeautifulSoup(page.content, 'html.parser')
         except:
             print("Error occured while updating")
             return()
-        i=0
-        while i < len(self.champs):
-            self.champs[i].configure(text=self.belts[i]+": ")
-            i+=1
-        self.getWrestlers(soup, "Male_Wrestlers", self.listbox_male)
-        self.getWrestlers(soup,"Female_Wrestlers",self.listbox_female)
-class NJPW(Company):
-    def __init__(self, name, url, parent_tab, belts):
-        Company.__init__(self, name, url, parent_tab, belts)
-        self.label_men.configure(text="Heavyweights")
-        self.label_women.configure(text="Junior Heavyweghts")
-        self.label_men.grid(row=1, column=0, padx=(81,0), pady=(5,0), sticky=W)
-        self.label_women.grid(row=1,column=1, padx=(50,0), pady=(5,0), sticky=W)
-    def getWrestlers(self, soup, category, listbox):
-        listbox.delete(0,END)
-        table = soup.find(id=category).findNext('table')
-        rows = table.findAll('tr')
-        for row in rows:
-            data = row.findAll('td')
-            if(len(data) > 0):
-                name = data[0].text.strip().split('[')[0]
-                listbox.insert(END, name)
-                notes = data[3].findAll('a')
-                if(len(notes) > 0):
-                    k=0
-                    for belt in self.belts:
-                        for note in notes:
-                            if (note.text.strip().split('[')[0] == belt or note.get('title') == belt ):
-                                self.champs[k].configure(text=self.champs[k]["text"]+name+"  ")
-                        k = k + 1
-    def update(self):
-        try:
-            page = requests.get(self.url)
-            soup = BeautifulSoup(page.content, 'html.parser')
-        except:
-            print("Error occured while updating")
+        
+        table = soup.find('table')
+        if not (table):
+            print("Could not find table")
             return()
-        i=0
-        while i < len(self.champs):
-            self.champs[i].configure(text=self.belts[i]+": ")
-            i+=1
-        self.getWrestlers(soup, "Heavyweight_division", self.listbox_male)
-        self.getWrestlers(soup,"Junior_heavyweight_division",self.listbox_female)
-class WWE(Company):
-    def __init__(self, name, url, parent_tab, belts, brand):
-        Company.__init__(self, name, url, parent_tab, belts)
-        self.brand = brand
-    def getWrestlers(self, soup, category, listbox):
-        listbox.delete(0,END)
-        if(self.brand == "SmackDown"):
-            category = category+"_2"
-        elif(self.brand == "NXT"):
-            category = category+"_3"
-        elif(self.brand == "NXT_UK"):
-            category = category+"_4"
-        table = soup.find(id=self.brand).findNext(id=category).findNext('table')
         rows = table.findAll('tr')
-        for row in rows:
-            data = row.findAll('td')
-            if(len(data) > 0):
-                name = data[0].text.strip().split('[')[0]
-                listbox.insert(END, name)
-                notes = data[2].findAll('a')
-                if(len(notes) > 0):
-                    k=0
-                    for belt in self.belts:
-                        for note in notes:
-                            if (note.text.strip().split('[')[0] == belt or note.get('title') == belt):
-                                self.champs[k].configure(text=self.champs[k]["text"]+name+"  ")
-                        k = k + 1
+        if not (rows):
+            print("Found table but no rows")
+            return()
+        
+        for brand in self.brands:
+            for row in rows:
+                cells = row.findAll('td')
+                if not (cells):
+                    continue
+                if(cells[4].text.casefold() == brand.name.casefold() and cells[3].text.find("Wrestler") >= 0):
+                    brand.listbox_roster.insert(END, cells[2].text)
+                    
+                
+        
+            
+                    
--- wrestling_roster_tool.py
+++ wrestling_roster_tool.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 from company import *
 
+"""
 def save():
 	for company in tabs:
 		company.save()
@@ -13,76 +14,61 @@ def load():
 		company.load()
 	for brand in wwe_tabs:
 		brand.load()
+"""
 def update():
-	for company in tabs:
-		company.update()
-	for brand in wwe_tabs:
-		brand.update()
+    for company in tabs:
+        company.update()
+    wweTab.update()
 
 # Main Window
 window = tkinter.Tk()
 window.title("Wrestling Roster Tool")
 window.geometry("800x600")
+
 menu = Menu(window)
 window.config(menu=menu)
 file = Menu(menu)
-file.add_command(label="Save All", command=save)
+#file.add_command(label="Save All", command=save)
 file.add_command(label="Update All", command=update)
 menu.add_cascade(label="File", menu=file)
 
 # Tab setup
-tab_parent = ttk.Notebook(window)
-	#Create companies
+parent_tab = ttk.Notebook(window)
+    #Create companies
 tabs = [
-Company("AAA",'https://en.wikipedia.org/wiki/List_of_Lucha_Libre_AAA_Worldwide_personnel',tab_parent,
+Company("AAA","https://www.cagematch.net/?id=8&nr=122&page=15",parent_tab,
 ["AAA Mega Champion","AAA Latin American Champion","AAA World Cruiserweight Champion","AAA Reina de Reinas Champion", 
 "AAA World Tag Team Champion","AAA World Trios Champion","AAA World Mixed Tag Team Champion"]),
-Company("AEW",'https://en.wikipedia.org/wiki/List_of_All_Elite_Wrestling_personnel',tab_parent,[
+Company("AEW","https://www.cagematch.net/?id=8&nr=2287&page=15",parent_tab,[
 "World Champion","Women's World Champion","TNT Champion","World Tag Team Champion","FTW Champion"]),
-Company("Impact",'https://en.wikipedia.org/wiki/List_of_Impact_Wrestling_personnel',tab_parent,[
+Company("Impact","https://www.cagematch.net/?id=8&nr=5&page=15",parent_tab,[
 "World Champion","Knockouts Champion","X Division Champion","World Tag Team Champion"]),
-MLW("MLW",'https://en.wikipedia.org/wiki/List_of_Major_League_Wrestling_personnel',tab_parent,[
+Company("MLW","https://www.cagematch.net/?id=8&nr=22&page=15",parent_tab,[
 "MLW World Heavyweight Champion","MLW World Middleweight Champion","MLW National Openweight Champion","MLW World Tag Team Champion"]),
-Company("NWA",'https://en.wikipedia.org/wiki/List_of_National_Wrestling_Alliance_personnel',tab_parent,[
+Company("NWA","https://www.cagematch.net/?id=8&nr=9&page=15",parent_tab,[
 "NWA Worlds Heavyweight Champion","NWA World Women's Champion","NWA National Heavyweight Champion","NWA World Tag Team Champion"]),
-NJPW("NJPW",'https://en.wikipedia.org/wiki/List_of_New_Japan_Pro-Wrestling_personnel',tab_parent,["IWGP Heavyweight Champion",
+Company("NJPW","https://www.cagematch.net/?id=8&nr=7&page=15",parent_tab,["IWGP Heavyweight Champion",
 "IWGP Intercontinental Champion","IWGP United States Champion","NEVER Openweight Champion","IWGP Tag Team Champion",
 "IWGP Junior Heavyweight Champion","IWGP Junior Heavyweight Tag Team Champion","NEVER Openweight 6-Man Tag Team Champion", "KOPW 2020"]),
-Company("ROH",'https://en.wikipedia.org/wiki/List_of_Ring_of_Honor_personnel',tab_parent,[
+Company("ROH","https://www.cagematch.net/?id=8&nr=4&page=15",parent_tab,[
 "World Champion","Pure Wrestling Champion","Television Champion","World Tag Team Champion","World Six-Man Tag Team Champion"])
 ]
-	#WWE contains brands so it becomes a parent tab
-wwe = ttk.Notebook(tab_parent)
-	#Create brands
-wwe_tabs = [
-WWE("RAW",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
-"WWE Champion","Raw Women's Champion","United States Champion","Raw Tag Team Champion","Women's Tag Team Champion","24/7 Champion"],"Raw"),
-WWE("Smackdown",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
-"Universal Champion","SmackDown Women's Champion","Intercontinental Champion","SmackDown Tag Team Champion",
-"Women's Tag Team Champion","24/7 Champion"],"SmackDown"),
-WWE("NXT",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
-"NXT Champion","NXT Women's Champion","North American Champion","NXT Tag Team Champion","Women's Tag Team Champion","NXT Cruiserweight Championship"], "NXT"),
-WWE("NXT UK",'https://en.wikipedia.org/wiki/List_of_WWE_personnel',wwe,[
-"NXT United Kingdom Champion","NXT UK Women's Champion","NXT UK Tag Team Champion", "NXT Cruiserweight Champion", "Heritage Cup Champion"], "NXT_UK")
-]
+wweTab = WWE(parent_tab)
 
-tab_parent.add(tabs[0].frame, text=tabs[0].name)
-tab_parent.add(tabs[1].frame, text=tabs[1].name)
-tab_parent.add(tabs[2].frame, text=tabs[2].name)
-tab_parent.add(tabs[3].frame, text=tabs[3].name)
-tab_parent.add(tabs[4].frame, text=tabs[4].name)
-tab_parent.add(tabs[5].frame, text=tabs[5].name)
-tab_parent.add(tabs[6].frame, text=tabs[6].name)
-tab_parent.add(wwe,text="WWE")
-wwe.add(wwe_tabs[0].frame,text=wwe_tabs[0].name)
-wwe.add(wwe_tabs[1].frame, text=wwe_tabs[1].name)
-wwe.add(wwe_tabs[2].frame, text=wwe_tabs[2].name)
-wwe.add(wwe_tabs[3].frame, text=wwe_tabs[3].name)
+parent_tab.add(tabs[0].frame, text=tabs[0].name)
+parent_tab.add(tabs[1].frame, text=tabs[1].name)
+parent_tab.add(tabs[2].frame, text=tabs[2].name)
+parent_tab.add(tabs[3].frame, text=tabs[3].name)
+parent_tab.add(tabs[4].frame, text=tabs[4].name)
+parent_tab.add(tabs[5].frame, text=tabs[5].name)
+parent_tab.add(tabs[6].frame, text=tabs[6].name)
 
-tab_parent.pack(expand=1, fill='both')
+parent_tab.add(wweTab.notebook,text="WWE")
+
+parent_tab.pack(expand=1, fill='both')
 
 #load rosters
-load()
+#load()
 
 #main loop
 window.mainloop()
